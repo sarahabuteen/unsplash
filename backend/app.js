73284b670461e53a8585c31f9cd5af51465b223f
Sarah Abuteen <sabuteen@terkwaz.com>
2020-11-11 22:38:44 +0200

    Set up db

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,21 @@
+const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
 require("dotenv").config();
+
+//db connection
+mongoose.connect(
+    process.env.MONGO_URI,
+    {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true
+    }
+).then(() => console.log('DB Connected'))
+
+mongoose.connection.on('error', err => {
+    console.log(`DB connection error: ${err.message}`)
+});
 
 
 const port = process.env.PORT || 8000;
